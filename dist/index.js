d087f322e2cf90b7ca340cc750cd3ae6ab8e3d1d
Fatih20 <fnri39@protonmail.com>
2022-07-02 15:58:12 +0700

    Fix lack of allow origin header from client error.

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,19 @@ dotenv.config();
 const app = (0, express_1.default)();
 const router = express_1.default.Router();
 // Middlewares
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', config_1.default.clientSite[0]);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 app.use((0, cors_1.default)({ credentials: true, origin: config_1.default.clientSite }));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,24 @@ const app:Application = express();
 const router: Router = express.Router();
 
 // Middlewares
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', serverConfig.clientSite[0]);
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(cors({ credentials: true, origin: serverConfig.clientSite}));
 app.use(express.json())
 app.use(cookieParser())
