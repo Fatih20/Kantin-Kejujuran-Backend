58f254a05ff501a49a151f9da92f14e1b1a42534
Fatih20 <fnri39@protonmail.com>
2022-07-02 19:06:46 +0700

    Attempt to connect straight to production database.

--- src/database/db.ts
+++ src/database/db.ts
@@ -5,12 +5,18 @@ const Pool = pg.Pool;
 
 const {user, password, host, name, port } = serverConfig.database
 
-const pool = new Pool({
-    user,
-    password,
-    host,
-    database : name,
-    port
-})
+// const pool = new Pool({
+//     user,
+//     password,
+//     host,
+//     database : name,
+//     port
+// })
+
+const proConfig = {
+    connectionString : serverConfig.database.url
+}
+
+const pool = new Pool (proConfig);
 
 export default pool;
