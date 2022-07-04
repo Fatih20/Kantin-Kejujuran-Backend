b788d8e35c32e26cc717eec80e0b7f603095f518
Fatih20 <fnri39@protonmail.com>
2022-07-05 06:20:49 +0700

    Give out error message to the checkIfLoggedIn middleware.

--- src/middleware/checkIfLoggedIn.ts
+++ src/middleware/checkIfLoggedIn.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 function checkIfLoggedIn (req : Request, res : Response, next : NextFunction) {
     if (res.locals.user === undefined || res.locals.user === null) {
-        return res.status(401).send({message : "You're not logged in"})
+        return res.status(401).send({error : "notLoggedIn",message : "You're not logged in"})
     }
     next();
 }
